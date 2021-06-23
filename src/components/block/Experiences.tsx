decb9b52c382eb5da9a0fa5cc52584080739a565
Reza Babaei <reza.tizro@gmail.com>
2021-06-23 03:10:17 +0300

    fix seperation line on text in printed pdf

--- src/components/block/Experiences.tsx
+++ src/components/block/Experiences.tsx
@@ -6,7 +6,7 @@ const Experience: FC<{ values: ExperienceBlockValueProps[] }> = ({ values }) => 
     return (
         <>
             {values.map(({ id, from, to, location, title, company, text, projects }) => (
-                <div className="mb-8 flex transform scale-90" key={id}>
+                <div className=" flex transform scale-90" key={id}>
                     <div className="w-1/4">
                         <p className="leading-normal text-md text-gray-650">
                             {from} - {to ? to : 'present'}
@@ -15,7 +15,7 @@ const Experience: FC<{ values: ExperienceBlockValueProps[] }> = ({ values }) => 
                         </p>
                     </div>
                     <div className="w-3/4">
-                        <h3 className="text-lg font-semibold text-gray-700 leading-snugish mb-4">
+                        <h3 className="text-lg font-semibold text-gray-700 leading-snugish mb-2">
                             {title}
                             <br />
                             <small>{company}</small>
